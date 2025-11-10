188cfd76dc5ca5749f687699475af5b35f64ea5c
Akhtar Abbas <akhtarabbas909@hotmail.com>
2025-11-11 02:17:48 +0500

    Implementation of Catalog API

--- ShoppingApp/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ ShoppingApp/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,6 +1,16 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
 {
-    public class CreateProductHandler
+
+    public record CreateProductCommand(string name, List<string> categories, string description, string imageFile, decimal price) 
+        : IRequest<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
     {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
