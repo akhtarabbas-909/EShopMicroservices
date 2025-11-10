ba65fb746079d83a71efb91a06f9b2784df21bc5
Akhtar Abbas <akhtarabbas909@hotmail.com>
2025-11-11 03:53:10 +0500

    implementation of Product Catalog

--- ShoppingApp/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ ShoppingApp/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,27 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using BuildingBlocks.CQRS;
+
+namespace Catalog.API.Products.CreateProduct
 {
-    public class CreateProductEndpoint
+    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    public record CreateProductResponst(Guid Id);
+    public class CreateProductEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products", async(CreateProductResult req, ISender sender) =>
+            {
+                var command = req.Adapt<CreateProductCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<CreateProductResponst>();
+
+                return Results.Created($"/products/{response.Id}", response);
+            })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponst>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product");
+            
+        }
     }
 }
